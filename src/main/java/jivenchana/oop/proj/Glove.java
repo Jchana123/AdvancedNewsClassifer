b691e92c43115da439e6095fa3e9d6a5a39f6f3a
Jchana123 <153704064+Jchana123@users.noreply.github.com>
2024-10-12 00:50:49 +0100

    Update Glove.java

--- src/main/java/jivenchana/oop/proj/Glove.java
+++ src/main/java/jivenchana/oop/proj/Glove.java
@@ -1,38 +1,33 @@
 package jivenchana.oop.proj;
 
-public class Glove {
-
+public class Glove 
+{
     private String strVocabulary;
     private Vector vecVector;
 
-    public Glove(String _vocabulary, Vector _vector) {
-
+    public Glove(String _vocabulary, Vector _vector) 
+    {
         strVocabulary = _vocabulary;
         vecVector = _vector;
-
     }
 
-    public String getVocabulary() {
-
+    public String getVocabulary() 
+    {
         return this.strVocabulary;
-
     }
 
-    public Vector getVector() {
-
+    public Vector getVector() 
+    {
         return this.vecVector;
-
     }
 
-    public void setVocabulary(String _vocabulary) {
-
+    public void setVocabulary(String _vocabulary) 
+    {
         this.strVocabulary = _vocabulary;
-
     }
 
-    public void setVector(Vector _vector) {
-
+    public void setVector(Vector _vector) 
+    {
         this.vecVector = _vector;
-
     }
 }
