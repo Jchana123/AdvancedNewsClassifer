8924303288b2fa652dbf454634517b2e7d9f8b46
Jchana123 <153704064+Jchana123@users.noreply.github.com>
2024-10-12 00:52:15 +0100

    Update HtmlParser.java

--- src/main/java/jivenchana/oop/proj/HtmlParser.java
+++ src/main/java/jivenchana/oop/proj/HtmlParser.java
@@ -1,27 +1,27 @@
 package jivenchana.oop.proj;
 
-public class HtmlParser {
+public class HtmlParser 
+{
     /***
      * Extract the title of the news from the _htmlCode.
      * @param _htmlCode Contains the full HTML string from a specific news. E.g. 01.htm.
      * @return Return the title if it's been found. Otherwise, return "Title not found!".
      */
 
-    public static String getNewsTitle(String _htmlCode) {
-
+    public static String getNewsTitle(String _htmlCode) 
+    {
         String titleTagOpen = "<title>";
         String titleTagClose = "</title>";
 
         int titleStart = _htmlCode.indexOf(titleTagOpen) + titleTagOpen.length();
         int titleEnd = _htmlCode.indexOf(titleTagClose);
 
-        if (titleStart != -1 && titleEnd != -1 && titleEnd > titleStart) {
+        if (titleStart != -1 && titleEnd != -1 && titleEnd > titleStart) 
+        {
             String strFullTitle = _htmlCode.substring(titleStart, titleEnd);
             return strFullTitle.substring(0, strFullTitle.indexOf(" |"));
         }
-
         return "Title not found!";
-
     }
 
     /***
@@ -29,24 +29,24 @@ public class HtmlParser {
      * @param _htmlCode Contains the full HTML string from a specific news. E.g. 01.htm.
      * @return Return the content if it's been found. Otherwise, return "Content not found!".
      */
-    public static String getNewsContent(String _htmlCode) {
-
+    
+    public static String getNewsContent(String _htmlCode) 
+    {
         String contentTagOpen = "\"articleBody\": \"";
         String contentTagClose = " \",\"mainEntityOfPage\":";
 
         int contentStart = _htmlCode.indexOf(contentTagOpen) + contentTagOpen.length();
         int contentEnd = _htmlCode.indexOf(contentTagClose);
 
-        if (contentStart != -1 && contentEnd != -1 && contentEnd > contentStart) {
+        if (contentStart != -1 && contentEnd != -1 && contentEnd > contentStart) 
+        {
             return _htmlCode.substring(contentStart, contentEnd).toLowerCase();
         }
-
         return "Content not found!";
-
     }
 
-    public static NewsArticles.DataType getDataType(String _htmlCode) {
-
+    public static NewsArticles.DataType getDataType(String _htmlCode) 
+    {
         String dataTypeOpenTag = "<datatype>";
         String dataTypeCloseTag = "</datatype>";
 
@@ -63,15 +63,14 @@ public class HtmlParser {
             if ("Training".equals(pulledOutDT))
             {
                 return NewsArticles.DataType.Training;
-
-            } return NewsArticles.DataType.Testing;
+            } 
+            return NewsArticles.DataType.Testing;
         }
         return NewsArticles.DataType.Testing;
-
     }
 
-    public static String getLabel (String _htmlCode) {
-
+    public static String getLabel (String _htmlCode) 
+    {
         String labelOpenTag = "<label>";
         String labelCloseTag = "</label>";
 
