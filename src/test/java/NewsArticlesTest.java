46155a0fe415a33bb96e915ec106a4c14644c98f
Jchana123 <153704064+Jchana123@users.noreply.github.com>
2024-10-12 01:02:55 +0100

    Update NewsArticlesTest.java

--- src/test/java/NewsArticlesTest.java
+++ src/test/java/NewsArticlesTest.java
@@ -3,10 +3,13 @@ import jivenchana.oop.proj.NewsArticles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class NewsArticlesTest {
+public class NewsArticlesTest 
+{
     @Test
-    void newsTest() {
+    void newsTest() 
+    {
         NewsArticles myArticles = new NewsArticles("Hi", "Hello world!", NewsArticles.DataType.Training, "-1");
+        
         assertEquals("Hi", myArticles.getNewsTitle());
         assertEquals("Hello world!", myArticles.getNewsContent());
         assertEquals(NewsArticles.DataType.Training, myArticles.getNewsType());
