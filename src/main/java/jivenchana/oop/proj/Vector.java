c84229dfcdc68d6401da8e9db612b344e5144276
Jchana123 <153704064+Jchana123@users.noreply.github.com>
2024-10-12 01:28:00 +0100

    Added Javadoc comments for Vector class methods

--- src/main/java/jivenchana/oop/proj/Vector.java
+++ src/main/java/jivenchana/oop/proj/Vector.java
@@ -1,14 +1,33 @@
 package jivenchana.oop.proj;
 
+/**
+ * Represents a mathematical vector and provides methods to manipulate and perform 
+ * vector operations such as addition, subtraction, dot product, and cosine similarity.
+ * 
+ * @author Jiven Chana 
+ * @version 1.0
+ * @since 2024
+ */
 public class Vector 
 {
     private double[] doubElements;
 
+    /**
+     * Constructor to initialize the vector with given elements.
+     * 
+     * @param _elements Array of elements for the vector
+     */
     public Vector(double[] _elements) 
     {
         doubElements = _elements;
     }
 
+    /**
+     * Retrieves the element at the specified index in the vector.
+     * 
+     * @param _index The index of the element
+     * @return The value at the specified index, or -1 if the index is out of bounds
+     */
     public double getElementatIndex(int _index) 
     {
         if (_index >= 0 && _index < doubElements.length)
@@ -17,6 +36,13 @@ public class Vector
             return -1;
     }
 
+    /**
+     * Sets the element at the specified index to the provided value.
+     * If the index is out of bounds, the last element is updated.
+     * 
+     * @param _value The value to set at the specified index
+     * @param _index The index to set the value at
+     */
     public void setElementatIndex(double _value, int _index) 
     {
         if (_index >= 0 && _index < doubElements.length)
@@ -25,16 +51,30 @@ public class Vector
             doubElements[doubElements.length - 1] = _value;
     }
 
+    /**
+     * @return An array of all elements in the vector
+     */
+
     public double[] getAllElements() 
     {
         return doubElements;
     }
 
+    /**
+     * @return The number of elements in the vector
+     */
     public int getVectorSize() 
     {
         return doubElements.length;
     }
 
+    /**
+     * Resizes the vector to the specified size. If the new size is larger, the additional 
+     * elements are initialized to -1.
+     * 
+     * @param _size The new size of the vector
+     * @return A new resized vector
+     */
     public Vector reSize(int _size) 
     {
         if (this.getVectorSize() == _size || _size <= 0)
@@ -53,6 +93,10 @@ public class Vector
         return new Vector(doubElementNew);
     }
 
+    /**
+     * @param _v The vector to be added
+     * @return A new vector which is the sum of this vector and the provided vector
+     */
     public Vector add(Vector _v) 
     {
         Vector myVector = this;
@@ -71,6 +115,10 @@ public class Vector
         return new Vector(doubElementNew);
     }
 
+    /**
+     * @param _v The vector to be subtracted
+     * @return A new vector which is the result of subtracting the provided vector from this vector
+     */
     public Vector subtraction(Vector _v) 
     {
         Vector myVector = this;
@@ -88,6 +136,10 @@ public class Vector
         return new Vector(doubElementNew);
     }
 
+    /**
+     * @param _v The vector with which to compute the dot product
+     * @return The dot product of the two vectors
+     */
     public double dotProduct(Vector _v) 
     {
         Vector myVector = this;
@@ -105,6 +157,10 @@ public class Vector
         return dotProduct;
     }
 
+    /**
+     * @param _v The vector to compare with
+     * @return The cosine similarity value between the two vectors
+     */
     public double cosineSimilarity(Vector _v) 
     {
         Vector myVector = this;
@@ -124,6 +180,10 @@ public class Vector
         return doubDProduct / (Math.sqrt(doubNorm1) * Math.sqrt(doubNorm2));
     }
 
+    /**
+     * @param _obj The object to compare this vector with
+     * @return {@code true} if the vectors are equal, {@code false} otherwise
+     */
     @Override
     public boolean equals(Object _obj) 
     {
@@ -144,6 +204,11 @@ public class Vector
         return boolEquals;
     }
 
+    /**
+     * Returns a string representation of the vector.
+     * 
+     * @return A comma-separated string of vector elements
+     */
     @Override
     public String toString() 
     {
