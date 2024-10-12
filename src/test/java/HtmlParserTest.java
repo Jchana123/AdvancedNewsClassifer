d02b1d78f855300934c9565d1c60cbdc8516b405
Jchana123 <153704064+Jchana123@users.noreply.github.com>
2024-10-12 01:02:32 +0100

    Update HtmlParserTest.java

--- src/test/java/HtmlParserTest.java
+++ src/test/java/HtmlParserTest.java
@@ -4,18 +4,21 @@ import jivenchana.oop.proj.NewsArticles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HtmlParserTest {
+public class HtmlParserTest 
+{
     private String strHTML = "<datatype>Training</datatype><label>2</label>";
     private String strHTML2 = "<datatype>Testing</datatype><label>1</label>";
 
     @Test
-    void getDataType() {
+    void getDataType() 
+    {
         assertEquals(NewsArticles.DataType.Training, HtmlParser.getDataType(strHTML));
         assertEquals(NewsArticles.DataType.Testing, HtmlParser.getDataType(strHTML2));
     }
 
     @Test
-    void getLabel() {
+    void getLabel() 
+    {
         assertEquals("2", HtmlParser.getLabel(strHTML));
         assertEquals("1", HtmlParser.getLabel(strHTML2));
     }
