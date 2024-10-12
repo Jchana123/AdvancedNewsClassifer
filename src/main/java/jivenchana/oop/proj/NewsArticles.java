717b967820440da226ef27918d73f9d3ddc25946
Jchana123 <153704064+Jchana123@users.noreply.github.com>
2024-10-12 01:40:49 +0100

    Added javadoc comments for NewsArticles class

--- src/main/java/jivenchana/oop/proj/NewsArticles.java
+++ src/main/java/jivenchana/oop/proj/NewsArticles.java
@@ -1,7 +1,14 @@
 package jivenchana.oop.proj;
 
+/**
+ * This class represents a news article with its title, content, type, and label.
+ */
 public class NewsArticles 
 {
+        
+    /**
+     * Enum representing the data type of the news article, either Training or Testing.
+     */
     public enum DataType 
     {
         Training, Testing
@@ -11,6 +18,14 @@ public class NewsArticles
     private DataType newsType = DataType.Testing;
     private String newsLabel = "-1";
 
+    /**
+     * Constructor to initialize a NewsArticles object.
+     * 
+     * @param _title The title of the news article.
+     * @param _content The content of the news article.
+     * @param _type The type of the news article (Training or Testing).
+     * @param _label The label associated with the news article.
+     */
     public NewsArticles(String _title, String _content, DataType _type, String _label) 
     {
         newsContent = _content;
@@ -19,11 +34,17 @@ public class NewsArticles
         newsLabel = _label;
     }
 
+    /**
+     * @return -> label of the news article
+     */
     public String getNewsLabel() 
     {
          return this.newsLabel;
     }
 
+    /**
+     * @return -> type of the news article (Training or Testing)
+     */
     public DataType getNewsType() 
     {
         return this.newsType;
