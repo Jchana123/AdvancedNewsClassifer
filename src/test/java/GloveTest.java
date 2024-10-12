12bf73203f6e6c7460f8e4198ff35da1038a3b70
Jchana123 <153704064+Jchana123@users.noreply.github.com>
2024-10-12 01:02:11 +0100

    Update GloveTest.java

--- src/test/java/GloveTest.java
+++ src/test/java/GloveTest.java
@@ -5,29 +5,34 @@ import jivenchana.oop.proj.Vector;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class GloveTest {
+class GloveTest 
+{
     String strWord1 = "HelloWorld";
     Vector vecVector = new Vector(new double[]{1,2,3});
     Glove myGlove = new Glove(strWord1, vecVector);
 
     @Test
-    void getVocabulary() {
+    void getVocabulary() 
+    {
         assertEquals("HelloWorld",myGlove.getVocabulary());
     }
 
     @Test
-    void setVocabulary() {
+    void setVocabulary() 
+    {
         myGlove.setVocabulary("ModifiedText");
         assertEquals("ModifiedText",myGlove.getVocabulary());
     }
 
    @Test
-    void getVector() {
+    void getVector() 
+    {
         assertEquals("1.00000,2.00000,3.00000",myGlove.getVector().toString());
     }
 
     @Test
-    void setVector() {
+    void setVector() 
+    {
         myGlove.setVector(new Vector(new double[]{4,5,6}));
         assertEquals("4.00000,5.00000,6.00000",myGlove.getVector().toString());
     }
